a420be6acd46eae29db186b32b374e2eae4e59f7
recokioshi <recokioshi@gmail.com>
2020-03-09 16:24:19 +0100

    Reorganized AppBar files

--- src/components/Home/AppBar/duck/AppBarOperations.ts
+++
@@ -1,9 +0,0 @@
-import firebaseApp from '../../../../common/Firebase/Firebase';
-
-export const handleLogOut = (handleLogoutFromParent: Function) => {
-    try {
-        firebaseApp.auth().signOut();
-    } catch (error) {
-        console.error(`sign out exception: ${error}`);
-    }
-};
